ec2624ac1b15e8ac5a77d23694d8b21d97e228d9
tranducanh2111 <tranducanh@sace.edu.vn>
2026-06-29 11:17:39 +1000

    feat(zalo): invert zalo svg to solid bubble with cutout Z

--- src/constants/media.ts
+++ src/constants/media.ts
@@ -20,7 +20,7 @@ export const ICON_PATHS = {
 		phone: "/icons/contact/phone.svg",
 		mail: "/icons/contact/mail.svg",
 		whatsapp: "/icons/contact/whatsapp.svg",
-		zalo: "/icons/contact/zalo-fixed4.svg",
+		zalo: "/icons/contact/zalo-fixed5.svg",
 	},
 	social: {
 		instagram: "/icons/social/instagram.svg",
