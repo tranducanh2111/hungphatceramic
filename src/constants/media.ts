d12e2888fd36d70cda05e26be1a759d4e23a950d
tranducanh2111 <tranducanh@sace.edu.vn>
2026-06-29 11:31:59 +1000

    feat(zalo): clean up redundant SVGs and set zalo.svg to outline word logo

--- src/constants/media.ts
+++ src/constants/media.ts
@@ -20,7 +20,7 @@ export const ICON_PATHS = {
 		phone: "/icons/contact/phone.svg",
 		mail: "/icons/contact/mail.svg",
 		whatsapp: "/icons/contact/whatsapp.svg",
-		zalo: "/icons/contact/zalo-fixed6.svg",
+		zalo: "/icons/contact/zalo.svg",
 	},
 	social: {
 		instagram: "/icons/social/instagram.svg",
